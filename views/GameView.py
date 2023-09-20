d7ac8c4c3b67cd8e07c8860fe4cceaa32251db6b
Bastien <bastien.corgnac@gmail.com>
2023-09-20 12:58:32 +0200

    new archi

---
+++ views/GameView.py
@@ -0,0 +1,87 @@
+import arcade
+from constant import *
+from Wall import Wall
+from Platform import Platform
+
+
+class GameView(arcade.View):
+    """
+    Main application class.
+    """
+
+    def __init__(self):
+
+        # Call the parent class and set up the window
+        super().__init__()
+
+        # Our Scene Object
+        self.scene = None
+
+        # Separate variable that holds the player sprite
+        self.platform = None
+
+        # Our physics engine
+        self.physics_engine = None
+
+
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+
+
+
+    def on_show_view(self):
+
+        # Initialize Scene
+        self.scene = arcade.Scene()
+
+        self.platform = Platform()
+        self.scene.add_sprite("Player", self.platform.sprite)
+
+        left_wall = Wall("./assets/lateral_wall.PNG", 0, SCREEN_HEIGHT / 2)
+        self.scene.add_sprite("Walls", left_wall.sprite)
+
+        right_wall = Wall("./assets/lateral_wall.PNG", SCREEN_WIDTH, SCREEN_HEIGHT / 2)
+        self.scene.add_sprite("Walls", right_wall.sprite)
+
+        right_wall = Wall("./assets/vertical_wall.PNG", SCREEN_WIDTH / 2, SCREEN_HEIGHT)
+        self.scene.add_sprite("Walls", right_wall.sprite)
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.platform.sprite, self.scene.get_sprite_list("Walls")
+        )
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+
+    def on_draw(self):
+        """Render the screen."""
+
+        # Clear the screen to the background color
+        self.clear()
+
+        # Draw our Scene
+        self.scene.draw()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.RIGHT or key == arcade.key.W:
+            self.platform.sprite.change_x = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.S:
+            self.platform.sprite.change_x = -PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key."""
+
+        if key == arcade.key.RIGHT or key == arcade.key.W:
+            self.platform.sprite.change_x = 0
+        elif key == arcade.key.LEFT or key == arcade.key.S:
+            self.platform.sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """Movement and game logic"""
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
---
+++ views/MenuView.py
@@ -0,0 +1,19 @@
+import arcade
+from constant import *
+from views.GameView import GameView
+
+
+class MenuView(arcade.View):
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        self.clear()
+        arcade.draw_text("Menu Screen", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+                         arcade.color.BLACK, font_size=50, anchor_x="center")
+        arcade.draw_text("Click to advance", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 75,
+                         arcade.color.GRAY, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = GameView()
+        self.window.show_view(game_view)
