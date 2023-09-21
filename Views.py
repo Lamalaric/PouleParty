7809239b6cf6761aaf99c3b938eb1e546b0a4c1e
Cleeemmm <clementine.mendoza@edu.esiee.fr>
2023-09-21 15:18:43 +0200

    menu Win

--- Views.py
+++ Views.py
@@ -373,14 +373,14 @@ class GameWinView(arcade.View):
 
 
     def on_show_view(self):
-        arcade.set_background_color(arcade.color.ORANGE_RED)
+        arcade.set_background_color(arcade.color.BLACK_OLIVE)
 
     def on_draw(self):
         self.clear()
         """
         Draw "Game over" across the screen.
         """
-        arcade.draw_text("Game Over", SCREEN_WIDTH/2, SCREEN_HEIGHT / 2, arcade.color.WHITE, 54,anchor_x="center")
+        arcade.draw_text("YOU WON !", SCREEN_WIDTH/2, SCREEN_HEIGHT / 2, arcade.color.WHITE, 54,anchor_x="center")
         arcade.draw_text("Click to restart", SCREEN_WIDTH/2, SCREEN_HEIGHT / 2 - 75, arcade.color.WHITE, 24,anchor_x="center")
 
         #timer
@@ -402,8 +402,8 @@ class GameWinView(arcade.View):
                          anchor_x="center")
 
         arcade.draw_text(f"Score : {self.score}",
-                         SCREEN_WIDTH / 2 - 100,
-                         250,
+                         SCREEN_WIDTH / 2 ,
+                         SCREEN_HEIGHT - 100,
                          arcade.color.GRAY,
                          font_size=15,
                          anchor_x="center")
