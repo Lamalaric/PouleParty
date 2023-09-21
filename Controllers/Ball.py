580177bdfb14b1a64389cdfd181bf02d3e90a6f3
Bastien <bastien.corgnac@gmail.com>
2023-09-21 12:53:47 +0200

    damage ball

--- Controllers/Ball.py
+++ Controllers/Ball.py
@@ -4,17 +4,21 @@ import arcade
 from arcade.color import RED
 from constant import *
 
+
 class Ball:
     def __init__(self):
-        self.sprite: arcade.Sprite = arcade.Sprite("./assets/egg.png", 0.05,texture=arcade.load_texture("./Assets/egg.png",width=512,height=512,hit_box_algorithm="Detailed"))# set ball position in the middle
+        self.sprite: arcade.Sprite = arcade.Sprite("./assets/egg.png", 0.05,
+                                                   texture=arcade.load_texture("./Assets/egg.png", width=512,
+                                                                               height=512,
+                                                                               hit_box_algorithm="Detailed"))  # set ball position in the middle
         self.sprite.position = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
         # randomize sprite direction and speed
         self.sprite.change_x = 0
         self.sprite.change_y = BALL_SPEED
-        self.damage = 1
+        self.damage = BALL_SCALE+1
 
     def modify_damage(self):
-        self.damage = 1 + 0.5 * self.sprite.width
+        self.damage = self.sprite.width // BALL_SCALE+1
         print(self.damage)
 
     def update(self):
