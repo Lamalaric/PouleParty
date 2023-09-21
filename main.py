1f062d1c643fbe9aa64ff6db74abf3c18df1ba02
Amalaric <lamalaric93@gmail.com>
2023-09-21 10:42:53 +0200

    Center window on application start

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Views import MenuView
 
 def main():
     """Main function"""
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Poule Party")
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Poule Party", center_window=True)
     menu_view = MenuView()
     window.show_view(menu_view)
     arcade.run()
